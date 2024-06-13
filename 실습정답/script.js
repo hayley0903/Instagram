1f16b08d2424455749ff029f514fe4030b4919ab
hayley0903 <hyejeonglee0903@gmail.com>
2024-06-13 11:17:15 +0900

    로그인 페이지

--- 실습정답/script.js
+++ 실습정답/script.js
@@ -9,14 +9,13 @@ function updateInputState(input, activeVar){
     input.parentElement.classList.remove('active');
     activeVar = false;
   }
-
+//return : 해당함수가 실행 된 후, 함수 호출한 위치로 전달되는 값
   return activeVar;
 }
 
 // //userid 의 자식이 input 요소 가져오기
 // let userid = document.getElementById('userid');
 // let useridInput = userid.querySelector('input');
-
 
 // let userpw = document.getElementById('userpw');
 // let userpwInput = userpw.querySelector('input');
@@ -37,10 +36,12 @@ function handleInput (e){
   
   if (type == "text"){
     idActive = updateInputState(input, idActive);
+    //return: activeVar값이 true일때, idActive가 true
 } else {
     pwActive = updateInputState(input, pwActive);
+     //return: activeVar값이 true일때, pwActive가 true
 }
-
+  //idActive와 pwActive가 true일경우, 둘다 값이 있을 때,
   if(idActive && pwActive) {
     submitBtn.removeAttribute('disabled');
   } else {
@@ -51,23 +52,96 @@ function handleInput (e){
 userid.addEventListener('keyup', handleInput);
 userpw.addEventListener('keyup', handleInput);
 
+//userid or userpw에 keyup됬을 때, handleInput함수가 실행
+//e.target = input으로 실행되면서, type이 text일때 id, type이 password일때 pw로 인식
 
+
+//e.target 속성//
+// let appTxt = document.querySelector('.app-txt');
+
+// function btnClick(e){
+//   console.log(e.target);
+// }
+
+// appTxt.addEventListener('click', btnClick);
+
+
+//비밀번호 표시 == > 비밀번호 숨기기로 변경//
+
+// let pwShow = document.querySelector('.pw-show');
+// let click = 0;
+
+// function pwShowClick (){
+//   click+=1;
+//   if (click % 2 != 0) {
+//     pwShow.innerHTML = "숨기기";
+//     userpw.type = "text";
+//   } else {
+//     pwShow.innerHTML = "비밀번호 표시";
+//     userpw.type = "password";
+//   }}
+
+// pwShow.addEventListener('click', pwShowClick )
+
+
+
+let pwShow = document.querySelector('.pw-show');
+
+// userpw의 type이 password일 경우, 1,2 번 실행
+// 1. 비밀번호 표시 클릭 => userpw(input) type => text로 변경
+// 2. pwShow.innerHTML => 숨기기로 변경
+
+
+function pwMode (){
+  if(userpw.getAttribute('type') == 'password'){
+    userpw.setAttribute('type', 'text');
+    pwShow.innerHTML = "숨기기";
+  } else {
+    userpw.setAttribute('type', 'password');
+    pwShow.innerHTML = "비밀번호 표시";
+  }
+}
+
+pwShow.addEventListener('click', pwMode);
+
+
+//Darkmode 구현//
+
+// let modeBtn = document.querySelector('.mode-btn');
+
+// function modeToggle (e){
+//   let body = document.querySelector('body');
+//   e.preventDefault();
+// //1. body태그에 dark라는 class toggle
+// //2. dark라는 class가 있으면 remove, 없으면 add
+//   body.classList.toggle('dark');
+//   //body에 dark라는 class가 있을 때, modeBtn.innerHTML을 lightmode로 변경
+//   //body에 dark라는 class가 없을 때는 darkmode로
+//   //classList.contains()로 클래스를 가지고 있는지 확인가능
+
+//   if(body.classList.contains('dark')) {
+//     modeBtn.innerHTML = 'Lightmode';
+//   } else {
+//     modeBtn.innerHTML = 'Darkmode';
+//   }}
+
+
+// modeBtn.addEventListener('click', modeToggle);
 
 
 let modeBtn = document.querySelector('.mode-btn');
-let count = 0;
 
-function modeToggle () {
+function modeToggle (e){
   let body = document.querySelector('body');
-  let modeTxt = modeBtn.innerHTML;
-  count+=1;
-  if(count % 2 == 0){
-    this.innerHTML = "Darkmode";
-    body.classList.remove('dark');
-  } else {
-    this.innerHTML = "Lightmode";
-    body.classList.add('dark');
-  }
+  e.preventDefault();
+//1. body태그에 dark라는 class toggle
+//2. dark라는 class가 있으면 remove, 없으면 add
+  body.classList.toggle('dark');
+
+  //삼항연산자
+  //조건 ? 참일 때 : 거짓일 때 
+  //body에 dark클래스가 있을때를 조건으로 두고 참일때 lightmode, 거짓일때 darkmode
+  modeBtn.innerHTML = body.classList.contains('dark') ? 'Lightmode' : 'Darkmode';
 }
 
 modeBtn.addEventListener('click', modeToggle);
